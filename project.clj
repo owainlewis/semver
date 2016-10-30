2c19cdbbbb94687cff9a77d9cf44740cd36e203e
Owain Lewis <owain@owainlewis.com>
2016-10-30 08:51:07 +0000

    Prepare for release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject io.forward/semver "0.1.0-SNAPSHOT"
+(defproject io.forward/semver "0.1.0"
   :description "Parser for semver specification"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/owainlewis/semver"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
